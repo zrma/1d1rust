883c6b5f9f6b1ae8e1269e051cdd6831d2c0e42b
zrma <bulbitain@gmail.com>
2019-12-12 01:36:07 +0900

    Add range based for example

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ mod sum;
 use sum::plus;
 
 fn main() {
+    let mut tot = 0;
+    for i in 1..11 {
+        tot += i;
+        println!("{}: {}", i, tot);
+    }
+    println!("tot: {}", tot);
+
     println!("Hello, world!");
     println!("{}", plus(3, 5));
 }
