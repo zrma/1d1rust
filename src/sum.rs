66b59293dfc070373f5c486f9adc7b247801b1b1
zrma <bulbitain@gmail.com>
2020-06-18 17:37:40 +0900

    Add range sum example

--- src/sum.rs
+++ src/sum.rs
@@ -1,12 +1,22 @@
 pub fn sum() {
     println!("{}", plus(3, 5));
+    println!("{}", agg(0, 100));
 }
 
 fn plus(a: i32, b: i32) -> i32 {
     a + b
 }
 
+fn agg(from: i32, to: i32) -> i32 {
+    (from..to).sum()
+}
+
 #[test]
 fn test_plus() {
     assert_eq!(plus(3, 5), 3 + 5)
+}
+
+#[test]
+fn test_agg() {
+    assert_eq!(agg(0, 11), 55)
 }
