81a334eda8876c78d595ae85fb9fbe777dc9ff64
zrma <bulbitain@gmail.com>
2020-06-21 22:06:16 +0900

    hacker_rank: solve plus_minus
    https://www.hackerrank.com/challenges/plus-minus/problem

--- src/hacker_rank/algorithms/warmup/mod.rs
+++ src/hacker_rank/algorithms/warmup/mod.rs
@@ -1,3 +1,4 @@
 mod birthday_cate_candles;
 mod diagonal_difference;
 mod mini_max_sum;
+mod plus_minus;
---
+++ src/hacker_rank/algorithms/warmup/plus_minus.rs
@@ -0,0 +1,29 @@
+#[allow(dead_code)]
+fn plus_minus(arr: Vec<i32>) -> (f64, f64, f64) {
+    let mut pos: i32 = 0;
+    let mut nag: i32 = 0;
+    let mut zero: i32 = 0;
+
+    arr.iter().for_each(|x| match *x {
+        n if n > 0 => pos += 1,
+        n if n < 0 => nag += 1,
+        _ => zero += 1,
+    });
+
+    let tot = arr.len() as f64;
+    (
+        f64::from(pos) / tot,
+        f64::from(nag) / tot,
+        f64::from(zero) / tot,
+    )
+}
+
+// https://www.hackerrank.com/challenges/plus-minus/problem
+#[test]
+fn test_plus_minus() {
+    let arr = std::vec![-4, 3, -9, 0, 4, 1];
+    let (pos, nag, zero) = plus_minus(arr);
+    assert!((pos - 3.0 / 6.0).abs() < f64::EPSILON);
+    assert!((nag - 2.0 / 6.0).abs() < f64::EPSILON);
+    assert!((zero - 1.0 / 6.0).abs() < f64::EPSILON);
+}
