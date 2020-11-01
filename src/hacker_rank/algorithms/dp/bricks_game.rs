322a400b49c9df3c720e9de6070977df4bb6a67c
zrma <bulbitain@gmail.com>
2020-11-01 17:28:11 +0900

    hacker_rank: do refactoring; hacker_rank/algorithms/dp/bricks_game

--- src/hacker_rank/algorithms/dp/bricks_game.rs
+++ src/hacker_rank/algorithms/dp/bricks_game.rs
@@ -15,91 +15,33 @@ fn test_min() {
     assert_eq!(min!(0, 1, -1), -1);
 }
 
-macro_rules! max {
-    ($x: expr) => ($x);
-    ($x: expr, $($xs: expr), +) => {
-        {
-            use std::cmp::max;
-            max($x, max!($($xs), +))
-        }
-    }
-}
-
-#[test]
-fn test_max() {
-    assert_eq!(max!(1), 1);
-    assert_eq!(max!(1, 3, 2), 3);
-    assert_eq!(max!(1, 0, -1), 1);
-}
-
-type Cache = std::collections::BTreeMap<usize, Inner>;
-type Inner = std::collections::BTreeMap<usize, i64>;
-
 #[allow(dead_code)]
 fn bricks_game(arr: Vec<i32>) -> i64 {
-    let mut cache = Cache::new();
-    play_sub_game(&arr, 0, arr.len() - 1, &mut cache)
-}
-
-fn get_cache(cache: &Cache, begin: usize, end: usize) -> (i64, bool) {
-    match cache.get(&begin) {
-        Some(inner) => match inner.get(&end) {
-            Some(x) => (*x, true),
-            _ => (0, false),
-        },
-        _ => (0, false),
-    }
-}
-
-fn set_cache(cache: &mut Cache, begin: usize, end: usize, val: i64) {
-    let c = cache.borrow_mut();
-    c.entry(begin).or_insert_with(Inner::new);
-    c.get_mut(&begin).unwrap().entry(end).or_insert(val);
-}
-
-fn play_sub_game(arr: &[i32], begin: usize, end: usize, cache: &mut Cache) -> i64 {
-    let (result, ok) = get_cache(cache, begin, end);
-    if ok {
-        return result;
+    let size = arr.len();
+    if size <= 3 {
+        return arr.iter().sum::<i32>() as i64;
     }
 
-    let range = end as i32 - begin as i32;
-    if range <= 3 {
-        let mut sum: i64 = 0;
-        for i in 0..min!(range + 1, 3) {
-            sum += arr[begin + i as usize] as i64;
-        }
-        set_cache(cache, begin, end, sum);
-        return sum;
+    let mut res = vec![0; size as usize];
+    let mut tot: i64 = 0;
+
+    let last_idx = size - 1;
+
+    for i in 0..3 {
+        tot += arr[last_idx - i] as i64;
+        res[last_idx - i] = tot;
     }
 
-    let _v0 = min!(
-        arr[begin] as i64 + play_sub_game(arr, begin + 2, end, cache),
-        arr[begin] as i64 + play_sub_game(arr, begin + 3, end, cache),
-        arr[begin] as i64 + play_sub_game(arr, begin + 4, end, cache)
-    );
-    let _v1 = min!(
-        arr[begin] as i64 + arr[begin + 1] as i64 + play_sub_game(arr, begin + 3, end, cache),
-        arr[begin] as i64 + arr[begin + 1] as i64 + play_sub_game(arr, begin + 4, end, cache),
-        arr[begin] as i64 + arr[begin + 1] as i64 + play_sub_game(arr, begin + 5, end, cache)
-    );
-    let _v2 = min!(
-        arr[begin] as i64
-            + arr[begin + 1] as i64
-            + arr[begin + 2] as i64
-            + play_sub_game(arr, begin + 4, end, cache),
-        arr[begin] as i64
-            + arr[begin + 1] as i64
-            + arr[begin + 2] as i64
-            + play_sub_game(arr, begin + 5, end, cache),
-        arr[begin] as i64
-            + arr[begin + 1] as i64
-            + arr[begin + 2] as i64
-            + play_sub_game(arr, begin + 6, end, cache)
-    );
-    let result = max!(_v0, _v1, _v2);
-    set_cache(cache, begin, end, result);
-    result
+    for i in 3..(last_idx + 1) {
+        tot += arr[last_idx - i] as i64;
+        res[last_idx - i] = tot
+            - min!(
+                res[last_idx - i + 1],
+                res[last_idx - i + 2],
+                res[last_idx - i + 3]
+            ) as i64;
+    }
+    res[0]
 }
 
 // https://www.hackerrank.com/challenges/play-game/problem
@@ -169,7 +111,6 @@ fn read_csv(file_name: &str) -> Vec<Vec<i32>> {
     res
 }
 
-use std::borrow::BorrowMut;
 use std::{sync::mpsc, thread, time::Duration};
 
 #[allow(dead_code)]
@@ -194,11 +135,9 @@ where
 
 #[test]
 fn test_bricks_game_bench() {
-    let arr = read_csv("./src/hacker_rank/algorithms/dp/test_data/bricks_game_0.csv");
-
-    // NOTE - 아래의 테스트 케이스는 stack overflow 발생하므로 비활성화
-    // let mut arr2 = read_csv("./src/hacker_rank/algorithms/dp/test_data/bricks_game_1.csv");
-    // arr.append(arr2.as_mut());
+    let mut arr = read_csv("./src/hacker_rank/algorithms/dp/test_data/bricks_game_0.csv");
+    let mut arr2 = read_csv("./src/hacker_rank/algorithms/dp/test_data/bricks_game_1.csv");
+    arr.append(arr2.as_mut());
 
     struct TestData {
         l: usize,
@@ -210,19 +149,18 @@ fn test_bricks_game_bench() {
         TestData { l: 1000, e: 249302 },
         TestData { l: 1000, e: 247737 },
         TestData { l: 1000, e: 253105 },
-        // NOTE - 위의 주석 참고
-        // TestData {
-        //     l: 100000,
-        //     e: 249791261588
-        // },
-        // TestData {
-        //     l: 100000,
-        //     e: 249894676936
-        // },
-        // TestData {
-        //     l: 100000,
-        //     e: 250224672758
-        // },
+        TestData {
+            l: 100000,
+            e: 249791261588
+        },
+        TestData {
+            l: 100000,
+            e: 249894676936
+        },
+        TestData {
+            l: 100000,
+            e: 250224672758
+        },
     ];
     assert_eq!(arr.len(), expected.len());
 
