a9e03fbe73c4173dd697147bbfdd65f3347567e3
zrma <bulbitain@gmail.com>
2023-05-17 22:44:50 +0900

    boj: solve 4889 안정적인 문자열
    - https://www.acmicpc.net/problem/4889

---
+++ src/boj/p4k/p4889.rs
@@ -0,0 +1,65 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve4889(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let mut case = 1;
+    loop {
+        let mut stack = Vec::new();
+        let mut res = 0;
+
+        let s = read_line(reader);
+        if s.contains('-') {
+            break;
+        }
+
+        for c in s.chars() {
+            if c == '{' {
+                stack.push(c);
+            } else if stack.is_empty() {
+                stack.push('{');
+                res += 1;
+            } else {
+                stack.pop();
+            }
+        }
+
+        res += stack.len() / 2;
+
+        writeln!(writer, "{}. {}", case, res).unwrap();
+        case += 1;
+    }
+}
+
+// https://www.acmicpc.net/problem/4889
+// 안정적인 문자열
+#[test]
+fn test_4889() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![TestData {
+        s: "}{
+{}{}{}
+{{{}
+---"
+        .to_string(),
+        want: "1. 2
+2. 0
+3. 1
+"
+        .to_string(),
+    }]
+    .iter()
+    .enumerate()
+    {
+        use std::io::Cursor;
+        let mut reader = Cursor::new(data.s.clone());
+        let mut writer = Cursor::new(Vec::new());
+        solve4889(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer.into_inner()).unwrap();
+        assert_eq!(data.want, got, "case {}", i);
+    }
+}
