0f673cf999bf5960457bbaed673725b6a991fd75
zrma <bulbitain@gmail.com>
2023-06-02 23:07:46 +0900

    boj: solve 2444 별 찍기 - 7
    - https://www.acmicpc.net/problem/2444

---
+++ src/boj/p2k/p2444.rs
@@ -0,0 +1,60 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve2444(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let n = read_line(reader).parse::<usize>().unwrap();
+
+    for i in 1..=n {
+        for _ in 0..(n - i) {
+            write!(writer, " ").unwrap();
+        }
+        for _ in 0..(2 * i - 1) {
+            write!(writer, "*").unwrap();
+        }
+        writeln!(writer).unwrap();
+    }
+    for i in (1..n).rev() {
+        for _ in 0..(n - i) {
+            write!(writer, " ").unwrap();
+        }
+        for _ in 0..(2 * i - 1) {
+            write!(writer, "*").unwrap();
+        }
+        writeln!(writer).unwrap();
+    }
+}
+
+// https://www.acmicpc.net/problem/2444
+// 별 찍기 - 7
+#[test]
+fn test_solve2444() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![TestData {
+        s: "5".to_string(),
+        want: "    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+"
+        .to_string(),
+    }]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve2444(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
