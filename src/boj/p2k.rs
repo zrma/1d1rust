dba0d332c73b0196df1334a1d70fcabc5ce72347
zrma <bulbitain@gmail.com>
2023-06-04 21:17:42 +0900

    boj: solve 2720 세탁소 사장 동혁
    - https://www.acmicpc.net/problem/2720

--- src/boj/p2k.rs
+++ src/boj/p2k.rs
@@ -4,6 +4,7 @@ mod p2437;
 mod p2444;
 mod p2583;
 mod p2671;
+mod p2720;
 mod p2870;
 mod p2954;
 mod p2998;
---
+++ src/boj/p2k/p2720.rs
@@ -0,0 +1,53 @@
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve2720(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+    let t = line.trim().parse::<usize>().unwrap();
+
+    for _ in 0..t {
+        let mut line = String::new();
+        reader.read_line(&mut line).unwrap();
+        let c = line.trim().parse::<u32>().unwrap();
+
+        let mut res = String::new();
+        res.push_str(&format!("{} ", c / 25));
+        res.push_str(&format!("{} ", (c % 25) / 10));
+        res.push_str(&format!("{} ", ((c % 25) % 10) / 5));
+        res.push_str(&format!("{}", ((c % 25) % 10) % 5));
+        writeln!(writer, "{}", res).unwrap();
+    }
+}
+
+// https://www.acmicpc.net/problem/2720
+// 세탁소 사장 동혁
+#[test]
+fn test_solve2720() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![TestData {
+        s: "3
+124
+25
+194"
+        .to_string(),
+        want: "4 2 0 4
+1 0 0 0
+7 1 1 4
+"
+        .to_string(),
+    }]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve2720(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
