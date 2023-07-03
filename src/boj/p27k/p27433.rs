95699bb1f224aed3c31f987503a023806b930f69
zrma <bulbitain@gmail.com>
2023-07-03 21:01:19 +0900

    boj: solve 27433 팩토리얼 2
    - https://www.acmicpc.net/problem/27433

---
+++ src/boj/p27k/p27433.rs
@@ -0,0 +1,58 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve27433(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let n = read_line(reader).parse::<u64>().unwrap();
+    let mut ans = 1;
+    for i in 1..=n {
+        ans *= i;
+    }
+    write!(writer, "{}", ans).unwrap();
+}
+
+// https://www.acmicpc.net/problem/27433
+// 팩토리얼 2
+#[test]
+fn test_solve27433() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![
+        TestData {
+            s: "10".to_string(),
+            want: "3628800".to_string(),
+        },
+        TestData {
+            s: "0".to_string(),
+            want: "1".to_string(),
+        },
+        TestData {
+            s: "1".to_string(),
+            want: "1".to_string(),
+        },
+        TestData {
+            s: "2".to_string(),
+            want: "2".to_string(),
+        },
+        TestData {
+            s: "3".to_string(),
+            want: "6".to_string(),
+        },
+        TestData {
+            s: "4".to_string(),
+            want: "24".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve27433(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
