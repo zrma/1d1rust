39840cdece3399a41e04fcab91adc6ae9cf6e892
zrma <bulbitain@gmail.com>
2023-07-20 22:22:51 +0900

    boj: solve 13235 팰린드롬
    - https://www.acmicpc.net/problem/13235

---
+++ src/boj/p13k/p13235.rs
@@ -0,0 +1,64 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve13235(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let s = read_line(reader);
+
+    let mut ans = true;
+    for i in 0..s.len() / 2 {
+        if s.chars().nth(i).unwrap() != s.chars().nth(s.len() - 1 - i).unwrap() {
+            ans = false;
+            break;
+        }
+    }
+
+    write!(writer, "{}", ans).unwrap();
+}
+
+// https://www.acmicpc.net/problem/13235
+// 팰린드롬
+// noinspection SpellCheckingInspection
+#[test]
+fn test_solve13235() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![
+        TestData {
+            s: "a".to_string(),
+            want: "true".to_string(),
+        },
+        TestData {
+            s: "uu".to_string(),
+            want: "true".to_string(),
+        },
+        TestData {
+            s: "owo".to_string(),
+            want: "true".to_string(),
+        },
+        TestData {
+            s: "bbbbbbbbbbb".to_string(),
+            want: "true".to_string(),
+        },
+        TestData {
+            s: "zzzzzzzzo".to_string(),
+            want: "false".to_string(),
+        },
+        TestData {
+            s: "slakdjfims".to_string(),
+            want: "false".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve13235(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "Failed test case {}", i);
+    }
+}
