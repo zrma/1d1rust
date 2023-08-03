c3b87b910faf5e8d53a8f26f164048dd8f3c0c6c
zrma <bulbitain@gmail.com>
2023-08-03 22:44:29 +0900

    boj: solve 5026 박사 과정
    - https://www.acmicpc.net/problem/5026

---
+++ src/boj/p5k/p5026.rs
@@ -0,0 +1,72 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve5026(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let n = read_line(reader).parse::<i32>().unwrap();
+
+    for _ in 0..n {
+        let s = read_line(reader);
+
+        if s == "P=NP" {
+            writeln!(writer, "skipped").unwrap();
+        } else {
+            let mut iter = s.split('+');
+            let a = iter.next().unwrap().parse::<i32>().unwrap();
+            let b = iter.next().unwrap().parse::<i32>().unwrap();
+            writeln!(writer, "{}", a + b).unwrap();
+        }
+    }
+}
+
+// https://www.acmicpc.net/problem/5026
+// 박사 과정
+#[test]
+fn test_solve5026() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![
+        TestData {
+            s: "4
+2+2
+1+2
+P=NP
+0+0"
+            .to_string(),
+            want: "4
+3
+skipped
+0
+"
+            .to_string(),
+        },
+        TestData {
+            s: "1
+3+5"
+            .to_string(),
+            want: "8
+"
+            .to_string(),
+        },
+        TestData {
+            s: "1
+P=NP"
+                .to_string(),
+            want: "skipped
+"
+            .to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve5026(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
