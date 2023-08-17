ec886416252bd6ea5e7e326906750acdf637a332
zrma <bulbitain@gmail.com>
2023-08-17 22:19:17 +0900

    boj: solve 3568 iSharp
    - https://www.acmicpc.net/problem/3568

--- src/boj/p3k.rs
+++ src/boj/p3k.rs
@@ -2,4 +2,5 @@ mod p3023;
 mod p3035;
 mod p3048;
 mod p3449;
+mod p3568;
 mod p3613;
---
+++ src/boj/p3k/p3568.rs
@@ -0,0 +1,61 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve3568(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let s = read_line(reader);
+    let mut iter = s.split_whitespace();
+
+    let default_type = iter.next().unwrap();
+    let mut vars = vec![];
+    for mut var in iter {
+        if var.ends_with(',') || var.ends_with(';') {
+            var = &var[..var.len() - 1];
+        }
+        vars.push(var);
+    }
+
+    for var in vars {
+        let var_name = var
+            .chars()
+            .take_while(|&ch| ch.is_alphabetic())
+            .collect::<String>();
+
+        let others = var.chars().skip(var_name.len()).collect::<String>();
+        if others.is_empty() {
+            writeln!(writer, "{} {};", default_type, var_name).unwrap();
+            continue;
+        }
+
+        let others = others.replace("[]", "][").chars().rev().collect::<String>();
+        writeln!(writer, "{}{} {};", default_type, others, var_name).unwrap();
+    }
+}
+
+// https://www.acmicpc.net/problem/3568
+// iSharp
+#[test]
+fn test_solve3568() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![TestData {
+        s: "int& a*[]&, b, c*;".to_string(),
+        want: "int&&[]* a;
+int& b;
+int&* c;
+"
+        .to_string(),
+    }]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve3568(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "case {}", i);
+    }
+}
