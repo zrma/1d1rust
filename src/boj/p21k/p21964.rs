cd95fcf2e0e6feae50ab0307369c3a67c615de39
zrma <bulbitain@gmail.com>
2023-08-31 21:55:42 +0900

    boj: solve 21964 선린인터넷고등학교 교가
    - https://www.acmicpc.net/problem/21964

---
+++ src/boj/p21k/p21964.rs
@@ -0,0 +1,46 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve21964(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let n = read_line(reader).parse::<usize>().unwrap();
+    let s = read_line(reader);
+
+    let ans = s.chars().skip(n - 5).take(5).collect::<String>();
+    write!(writer, "{}", ans).unwrap();
+}
+
+// https://www.acmicpc.net/problem/21964
+// noinspection SpellCheckingInspection
+// 선린인터넷고등학교 교가
+#[test]
+fn test_solve21964() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![
+        TestData {
+            s: "12
+Sunrin,Hair."
+                .to_string(),
+            want: "Hair.".to_string(),
+        },
+        TestData {
+            s: "5
+Sunrin,High."
+                .to_string(),
+            want: "Sunri".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve21964(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "Failed test case {}", i);
+    }
+}
