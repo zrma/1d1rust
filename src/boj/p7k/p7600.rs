7b0a0457f145576c9f9dbe8e6e3053256f98e566
zrma <bulbitain@gmail.com>
2023-09-05 23:03:01 +0900

    boj: solve 7600 문자가 몇갤까
    - https://www.acmicpc.net/problem/7600

---
+++ src/boj/p7k/p7600.rs
@@ -0,0 +1,69 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve7600(reader: &mut impl BufRead, writer: &mut impl Write) {
+    loop {
+        let s = read_line(reader);
+        if s == "#" {
+            break;
+        }
+
+        let ans = s
+            .as_bytes()
+            .iter()
+            .filter(|&x| x.is_ascii_alphabetic())
+            .map(|&x| x.to_ascii_lowercase() - b'a')
+            .fold([false; 26], |mut acc, x| {
+                acc[x as usize] = true;
+                acc
+            })
+            .iter()
+            .filter(|&x| *x)
+            .count();
+
+        writeln!(writer, "{}", ans).unwrap();
+    }
+}
+
+// https://www.acmicpc.net/problem/7600
+// 문자가 몇갤까
+#[test]
+fn test_solve7600() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![
+        TestData {
+            s: "The quick brown fox jumped over the lazy dogs.
+2 + 2 = 4
+New Zealand Programming Contest.
+#"
+            .to_string(),
+            want: "26
+0
+16
+"
+            .to_string(),
+        },
+        TestData {
+            s: "The quick brown fox jumps over the lazy dog
+#"
+            .to_string(),
+            want: "26
+"
+            .to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve7600(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
