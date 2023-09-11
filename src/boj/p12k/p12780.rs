63eb7352ae6c92b7b9e39a49cea0b8e0a32fc699
zrma <bulbitain@gmail.com>
2023-09-11 21:39:35 +0900

    boj: solve 12780 원피스
    - https://www.acmicpc.net/problem/12780

---
+++ src/boj/p12k/p12780.rs
@@ -0,0 +1,52 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve12780(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let s = read_line(reader);
+    let t = read_line(reader);
+
+    let mut ans = 0;
+    for i in 0..s.len() - t.len() + 1 {
+        if s[i..i + t.len()] == t {
+            ans += 1;
+        }
+    }
+
+    write!(writer, "{}", ans).unwrap();
+}
+
+// https://www.acmicpc.net/problem/12780
+// 원피스
+// noinspection SpellCheckingInspection
+#[test]
+fn test_solve12780() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![
+        TestData {
+            s: "INHAUNIVERSITYISONEOFTHEBESTUNIVERSITYINTHEWORLD
+UNIVERSITY"
+                .to_string(),
+            want: "2".to_string(),
+        },
+        TestData {
+            s: "INHAUNIVERSITYISONEOFTHEBESTUNIVERSITYINTHEWORLD
+THE"
+            .to_string(),
+            want: "2".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve12780(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "Failed test case {}", i);
+    }
+}
