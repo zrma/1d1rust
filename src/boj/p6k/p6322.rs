09d7caa3c32382bec42e726ac65f1a8281b0750e
zrma <bulbitain@gmail.com>
2023-09-22 23:54:21 +0900

    boj: solve 6322 직각 삼각형의 두 변
    - https://www.acmicpc.net/problem/6322

---
+++ src/boj/p6k/p6322.rs
@@ -0,0 +1,100 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve6322(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let mut i = 1;
+    loop {
+        let s = read_line(reader);
+        let (a, b, c) = {
+            let mut s = s.split_whitespace();
+            (
+                s.next().unwrap().parse::<i32>().unwrap(),
+                s.next().unwrap().parse::<i32>().unwrap(),
+                s.next().unwrap().parse::<i32>().unwrap(),
+            )
+        };
+
+        if a == 0 && b == 0 && c == 0 {
+            break;
+        }
+
+        let fa = a as f64;
+        let fb = b as f64;
+        let fc = c as f64;
+
+        writeln!(writer, "Triangle #{}", i).unwrap();
+
+        match (a, b, c) {
+            (-1, _, _) => {
+                if c * c - b * b <= 0 {
+                    writeln!(writer, "Impossible.").unwrap();
+                } else {
+                    writeln!(writer, "a = {:.3}", (fc * fc - fb * fb).sqrt()).unwrap();
+                }
+            }
+            (_, -1, _) => {
+                if c * c - a * a <= 0 {
+                    writeln!(writer, "Impossible.").unwrap();
+                } else {
+                    writeln!(writer, "b = {:.3}", (fc * fc - fa * fa).sqrt()).unwrap();
+                }
+            }
+            _ => writeln!(writer, "c = {:.3}", (fa * fa + fb * fb).sqrt()).unwrap(),
+        }
+
+        writeln!(writer).unwrap();
+
+        i += 1;
+    }
+}
+
+// https://www.acmicpc.net/problem/6322
+// 직각 삼각형의 두 변
+#[test]
+fn test_solve6322() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![
+        TestData {
+            s: "3 4 -1
+-1 2 7
+5 -1 3
+0 0 0"
+                .to_string(),
+            want: "Triangle #1
+c = 5.000
+
+Triangle #2
+a = 6.708
+
+Triangle #3
+Impossible.
+
+"
+            .to_string(),
+        },
+        TestData {
+            s: "3 4 -1
+0 0 0 "
+                .to_string(),
+            want: "Triangle #1
+c = 5.000
+
+"
+            .to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve6322(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
