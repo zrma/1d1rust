9e2ee43c8d5305c545640f7289e9f56bf3c9724f
zrma <bulbitain@gmail.com>
2023-09-27 22:13:27 +0900

    boj: solve 15973 두 박스
    - https://www.acmicpc.net/problem/15973

---
+++ src/boj/p15k/p15973.rs
@@ -0,0 +1,102 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve15973(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let (ax, ay, ap, aq) = {
+        let s = read_line(reader);
+        let mut iter = s.split_whitespace();
+        (
+            iter.next().unwrap().parse::<i32>().unwrap(),
+            iter.next().unwrap().parse::<i32>().unwrap(),
+            iter.next().unwrap().parse::<i32>().unwrap(),
+            iter.next().unwrap().parse::<i32>().unwrap(),
+        )
+    };
+
+    let (bx, by, bp, bq) = {
+        let s = read_line(reader);
+        let mut iter = s.split_whitespace();
+        (
+            iter.next().unwrap().parse::<i32>().unwrap(),
+            iter.next().unwrap().parse::<i32>().unwrap(),
+            iter.next().unwrap().parse::<i32>().unwrap(),
+            iter.next().unwrap().parse::<i32>().unwrap(),
+        )
+    };
+
+    let res = common_point_code((ax, ay, ap, aq), (bx, by, bp, bq));
+    write!(writer, "{}", res).unwrap();
+}
+
+type Rect = (i32, i32, i32, i32);
+
+fn common_point_code(rect1: Rect, rect2: Rect) -> String {
+    const DISJOINT: &str = "NULL";
+    const COMMON_POINT: &str = "POINT";
+    const COMMON_LINE: &str = "LINE";
+    const COMMON_AREA: &str = "FACE";
+
+    let (ax, ay, ap, aq) = rect1;
+    let (bx, by, bp, bq) = rect2;
+
+    if ax > bp || ay > bq || bx > ap || by > aq {
+        return DISJOINT.to_string();
+    }
+
+    if (ax == bp || ap == bx) && (ay == bq || aq == by) {
+        return COMMON_POINT.to_string();
+    }
+
+    if (ax == bp || ap == bx) || (ay == bq || aq == by) {
+        return COMMON_LINE.to_string();
+    }
+
+    COMMON_AREA.to_string()
+}
+
+// https://www.acmicpc.net/problem/15973
+// 두 박스
+#[test]
+fn test_solve15973() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![
+        TestData {
+            s: "1 2 3 5
+3 5 8 11"
+                .to_string(),
+            want: "POINT".to_string(),
+        },
+        TestData {
+            s: "3 5 9 10
+5 4 8 11"
+                .to_string(),
+            want: "FACE".to_string(),
+        },
+        TestData {
+            s: "1 2 3 5
+9 3 12 7"
+                .to_string(),
+            want: "NULL".to_string(),
+        },
+        TestData {
+            s: "3 5 9 10
+9 3 12 7"
+                .to_string(),
+            want: "LINE".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve15973(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
