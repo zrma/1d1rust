307df59377f10dd2e499921e3c4ad34f2aebb8fb
zrma <bulbitain@gmail.com>
2023-09-28 19:25:03 +0900

    boj: solve 9366 삼각형 분류
    - https://www.acmicpc.net/problem/9366

---
+++ src/boj/p9k/p9366.rs
@@ -0,0 +1,85 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve9366(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let t = read_line(reader).parse::<usize>().unwrap();
+    for i in 0..t {
+        let (a, b, c) = {
+            let s = read_line(reader);
+            let mut iter = s.split_whitespace();
+            (
+                iter.next().unwrap().parse::<i32>().unwrap(),
+                iter.next().unwrap().parse::<i32>().unwrap(),
+                iter.next().unwrap().parse::<i32>().unwrap(),
+            )
+        };
+
+        let res = triangle_type(a, b, c);
+        writeln!(writer, "Case #{}: {}", i + 1, res).unwrap();
+    }
+}
+
+fn triangle_type(p0: i32, p1: i32, p2: i32) -> String {
+    let mut v = vec![p0, p1, p2];
+    v.sort();
+    let (a, b, c) = (v[0], v[1], v[2]);
+
+    if a + b <= c {
+        return "invalid!".to_string();
+    }
+
+    if a == b && b == c {
+        return "equilateral".to_string();
+    }
+
+    if a == b || b == c {
+        return "isosceles".to_string();
+    }
+
+    "scalene".to_string()
+}
+
+// https://www.acmicpc.net/problem/9366
+// 삼각형 분류
+#[test]
+fn test_solve9366() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![
+        TestData {
+            s: "2
+3 3 4
+6 4 2"
+                .to_string(),
+            want: "Case #1: isosceles
+Case #2: invalid!
+"
+            .to_string(),
+        },
+        TestData {
+            s: "3
+3 4 5
+4 4 4
+4 3 2"
+                .to_string(),
+            want: "Case #1: scalene
+Case #2: equilateral
+Case #3: scalene
+"
+            .to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve9366(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "Failed test case {}", i);
+    }
+}
