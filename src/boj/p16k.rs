05512ba9fa1e7157055515ca93c03a51d906b651
zrma <bulbitain@gmail.com>
2023-10-05 12:44:40 +0900

    boj: solve 16479 컵라면 측정하기
    - https://www.acmicpc.net/problem/16479

--- src/boj/p16k.rs
+++ src/boj/p16k.rs
@@ -1,5 +1,6 @@
 mod p16120;
 mod p16171;
+mod p16479;
 mod p16486;
 mod p16600;
 mod p16693;
---
+++ src/boj/p16k/p16479.rs
@@ -0,0 +1,69 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve16479(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let k = read_line(reader).parse::<i32>().unwrap();
+    let (d1, d2) = {
+        let s = read_line(reader);
+        let mut iter = s.split_whitespace();
+        (
+            iter.next().unwrap().parse::<i32>().unwrap(),
+            iter.next().unwrap().parse::<i32>().unwrap(),
+        )
+    };
+
+    let res = match d1.cmp(&d2) {
+        std::cmp::Ordering::Equal => k.pow(2) as f64,
+        std::cmp::Ordering::Less => {
+            let d3 = d2 - d1;
+            k.pow(2) as f64 - (d3.pow(2) as f64) / 4.0
+        }
+        std::cmp::Ordering::Greater => {
+            let d3 = d1 - d2;
+            k.pow(2) as f64 - (d3.pow(2) as f64) / 4.0
+        }
+    };
+
+    write!(writer, "{}", res).unwrap();
+}
+
+// https://www.acmicpc.net/problem/16479
+// 컵라면 측정하기
+#[test]
+fn test_solve16479() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![
+        TestData {
+            s: "14
+12 12"
+                .to_string(),
+            want: "196".to_string(),
+        },
+        TestData {
+            s: "8
+9 3"
+            .to_string(),
+            want: "55".to_string(),
+        },
+        TestData {
+            s: "15
+13 6"
+                .to_string(),
+            want: "212.75".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve16479(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "case {}", i);
+    }
+}
