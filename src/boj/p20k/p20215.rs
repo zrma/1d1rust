2489eec82b9dc08f20033eb9fd2df65625303377
zrma <bulbitain@gmail.com>
2023-10-09 13:32:47 +0900

    boj: solve 20215 Cutting Corners
    - https://www.acmicpc.net/problem/20215

---
+++ src/boj/p20k/p20215.rs
@@ -0,0 +1,64 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve20215(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let (w, h) = {
+        let s = read_line(reader);
+        let mut iter = s.split_whitespace();
+        (
+            iter.next().unwrap().parse::<f64>().unwrap(),
+            iter.next().unwrap().parse::<f64>().unwrap(),
+        )
+    };
+
+    let ans = (w + h) - (w * w + h * h).sqrt();
+    write!(writer, "{}", ans).unwrap();
+}
+
+// https://www.acmicpc.net/problem/20215
+// Cutting Corners
+#[test]
+fn test_solve20215() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![
+        TestData {
+            s: "3 4".to_string(),
+            want: "2".to_string(),
+        },
+        TestData {
+            s: "12 7".to_string(),
+            want: "5.107556011".to_string(),
+        },
+        TestData {
+            s: "1 1".to_string(),
+            want: "0.585786438".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve20215(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap().parse::<f64>().unwrap();
+        let want = data.want.parse::<f64>().unwrap();
+
+        const EPSILON: f64 = 1e-6;
+
+        let abs_diff = (got - want).abs();
+        let rel_diff = abs_diff / want.abs().max(1e-9);
+
+        assert!(
+            abs_diff < EPSILON || rel_diff < EPSILON,
+            "case {}: absolute error: {}, relative error: {}",
+            i,
+            abs_diff,
+            rel_diff
+        );
+    }
+}
