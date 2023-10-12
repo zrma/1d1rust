e4582b2f95f3aa7f542261da3eccc9fc53248d46
zrma <bulbitain@gmail.com>
2023-10-12 23:49:30 +0900

    boj: solve 16485 작도하자! - ②
    - https://www.acmicpc.net/problem/16485

---
+++ src/boj/p16k/p16485.rs
@@ -0,0 +1,60 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve16486(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let (c, b) = {
+        let s = read_line(reader);
+        let mut iter = s.split_whitespace();
+        (
+            iter.next().unwrap().parse::<f64>().unwrap(),
+            iter.next().unwrap().parse::<f64>().unwrap(),
+        )
+    };
+
+    let ans = c / b;
+    write!(writer, "{}", ans).unwrap();
+}
+
+// https://www.acmicpc.net/problem/16486
+// 작도하자! - ②
+#[test]
+fn test_solve16486() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in vec![
+        TestData {
+            s: "9 3".to_string(),
+            want: "3".to_string(),
+        },
+        TestData {
+            s: "15 7".to_string(),
+            want: "2.1428571428".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve16486(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap().parse::<f64>().unwrap();
+        let want = data.want.parse::<f64>().unwrap();
+
+        const EPSILON: f64 = 1e-6;
+
+        let abs_diff = (got - want).abs();
+        let rel_diff = abs_diff / want.abs().max(1e-9);
+
+        assert!(
+            abs_diff < EPSILON || rel_diff < EPSILON,
+            "case {}: absolute error: {}, relative error: {}",
+            i,
+            abs_diff,
+            rel_diff
+        );
+    }
+}
