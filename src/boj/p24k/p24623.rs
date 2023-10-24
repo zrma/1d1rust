7502616b88d0c3016cf9b36d2f75ebc1ed693af1
zrma <bulbitain@gmail.com>
2023-10-24 21:04:49 +0900

    boj: solve 24623 Изгороди
    - https://www.acmicpc.net/problem/24623

---
+++ src/boj/p24k/p24623.rs
@@ -0,0 +1,44 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve24723(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let _ = read_line(reader).parse::<i32>().unwrap();
+    let _ = read_line(reader).parse::<i32>().unwrap();
+
+    write!(writer, "90").unwrap();
+}
+
+// https://www.acmicpc.net/problem/24723
+// Изгороди
+#[test]
+fn test_solve24723() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "2
+90"
+            .to_string(),
+            want: "90".to_string(),
+        },
+        TestData {
+            s: "123
+180"
+            .to_string(),
+            want: "90".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve24723(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
