267ae8d71e826f05ae8a155a23e476ddf3350312
zrma <bulbitain@gmail.com>
2023-11-12 13:26:30 +0900

    boj: solve 14940 쉬운 최단거리
    - https://www.acmicpc.net/problem/14940

---
+++ src/boj/p14k/p14940.rs
@@ -0,0 +1,151 @@
+use crate::read_values;
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve14940(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let (n, m) = read_values!(read_line(reader), usize, usize);
+    let mut map = vec![vec![0; m]; n];
+
+    let mut line = String::new();
+
+    let mut start = (0, 0);
+    for (i, row) in map.iter_mut().enumerate() {
+        line.clear();
+        reader.read_line(&mut line).unwrap();
+
+        for (j, c) in line.split_whitespace().enumerate() {
+            let v = c.parse::<i32>().unwrap();
+            match v {
+                2 => {
+                    start = (i, j);
+                    row[j] = 0;
+                }
+                0 => row[j] = 0,
+                _ => row[j] = -1,
+            }
+        }
+    }
+
+    let mut queue = std::collections::VecDeque::new();
+    queue.push_back(start);
+
+    while let Some((x, y)) = queue.pop_front() {
+        let dist = map[x][y];
+        for (dx, dy) in [(0, 1), (0, -1), (1, 0), (-1, 0)].iter() {
+            let nx = x as i32 + dx;
+            let ny = y as i32 + dy;
+            if nx < 0 || nx >= n as i32 || ny < 0 || ny >= m as i32 {
+                continue;
+            }
+
+            let (nx, ny) = (nx as usize, ny as usize);
+            if map[nx][ny] >= 0 {
+                continue;
+            }
+
+            map[nx][ny] = dist + 1;
+            queue.push_back((nx, ny));
+        }
+    }
+
+    let result = map
+        .iter()
+        .map(|row| row.iter().map(i32::to_string).collect::<Vec<_>>().join(" "))
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    write!(writer, "{}", result).expect("Failed to write");
+}
+
+// https://www.acmicpc.net/problem/14940
+// 쉬운 최단거리
+#[test]
+fn test_solve14940() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "15 15
+2 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 0 0 0 0 1
+1 1 1 1 1 1 1 1 1 1 0 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 0 1 0 0 0
+1 1 1 1 1 1 1 1 1 1 0 1 1 1 1"
+                .to_string(),
+            want: "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+3 4 5 6 7 8 9 10 11 12 13 14 15 16 17
+4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
+8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
+9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+11 12 13 14 15 16 17 18 19 20 0 0 0 0 25
+12 13 14 15 16 17 18 19 20 21 0 29 28 27 26
+13 14 15 16 17 18 19 20 21 22 0 30 0 0 0
+14 15 16 17 18 19 20 21 22 23 0 31 32 33 34"
+                .to_string(),
+        },
+        TestData {
+            s: "15 15
+2 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 0 0 0 0 0
+1 1 1 1 1 1 1 1 1 1 0 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 0 1 0 0 0
+1 1 1 1 1 1 1 1 1 1 0 1 1 1 1"
+                .to_string(),
+            want: "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+3 4 5 6 7 8 9 10 11 12 13 14 15 16 17
+4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
+8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
+9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+11 12 13 14 15 16 17 18 19 20 0 0 0 0 0
+12 13 14 15 16 17 18 19 20 21 0 -1 -1 -1 -1
+13 14 15 16 17 18 19 20 21 22 0 -1 0 0 0
+14 15 16 17 18 19 20 21 22 23 0 -1 -1 -1 -1"
+                .to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve14940(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
