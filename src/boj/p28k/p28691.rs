c4205b9300b3137b556ce254a5d985040c86b9c6
zrma <bulbitain@gmail.com>
2023-12-18 07:54:39 +0900

    boj: solve 28691 정보보호학부 동아리 소개
    - https://www.acmicpc.net/problem/28691

---
+++ src/boj/p28k/p28691.rs
@@ -0,0 +1,60 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve28691(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let s = read_line(reader);
+    let ans = match s.as_str() {
+        "M" => "MatKor",
+        "W" => "WiCys",
+        "C" => "CyKor",
+        "A" => "AlKor",
+        "$" => "$clear",
+        _ => "",
+    };
+
+    write!(writer, "{}", ans).unwrap();
+}
+
+// https://www.acmicpc.net/problem/28691
+// noinspection SpellCheckingInspection
+// 정보보호학부 동아리 소개
+#[test]
+fn test_solve28691() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "M".to_string(),
+            want: "MatKor".to_string(),
+        },
+        TestData {
+            s: "W".to_string(),
+            want: "WiCys".to_string(),
+        },
+        TestData {
+            s: "C".to_string(),
+            want: "CyKor".to_string(),
+        },
+        TestData {
+            s: "A".to_string(),
+            want: "AlKor".to_string(),
+        },
+        TestData {
+            s: "$".to_string(),
+            want: "$clear".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve28691(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
