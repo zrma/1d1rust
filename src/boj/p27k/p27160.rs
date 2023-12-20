84d4afc7933fade2009db626e7b07a4e84cbd992
zrma <bulbitain@gmail.com>
2023-12-21 08:47:44 +0900

    boj: solve 27160 할리갈리
    - https://www.acmicpc.net/problem/27160

---
+++ src/boj/p27k/p27160.rs
@@ -0,0 +1,78 @@
+use crate::read_values;
+use crate::utils::io::{read_line, read_value};
+use std::collections::HashMap;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve27160(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let n = read_value(read_line(reader));
+
+    let mut counts = HashMap::new();
+    for _ in 0..n {
+        let (name, count) = read_values!(read_line(reader), String, u32);
+        *counts.entry(name).or_insert(0) += count;
+    }
+
+    let answer = counts.values().any(|&count| count == 5);
+    write!(writer, "{}", if answer { "YES" } else { "NO" }).unwrap();
+}
+
+// https://www.acmicpc.net/problem/27160
+// noinspection SpellCheckingInspection
+// 할리갈리
+#[test]
+fn test_solve27160() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "3
+BANANA 2
+PLUM 4
+BANANA 3"
+                .to_string(),
+            want: "YES".to_string(),
+        },
+        TestData {
+            s: "4
+STRAWBERRY 1
+BANANA 2
+LIME 3
+PLUM 4"
+                .to_string(),
+            want: "NO".to_string(),
+        },
+        TestData {
+            s: "2
+LIME 5
+LIME 1"
+                .to_string(),
+            want: "NO".to_string(),
+        },
+        TestData {
+            s: "2
+BANANA 5
+BANANA 5"
+                .to_string(),
+            want: "NO".to_string(),
+        },
+        TestData {
+            s: "1
+BANANA 5"
+                .to_string(),
+            want: "YES".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve27160(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
