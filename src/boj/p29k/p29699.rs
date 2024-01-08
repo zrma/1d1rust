92b3fd219f531ababd3733d94dccc1676e541d51
zrma <bulbitain@gmail.com>
2024-01-09 08:45:07 +0900

    boj: solve 29699 Welcome to SMUPC!
    - https://www.acmicpc.net/problem/29699

---
+++ src/boj/p29k/p29699.rs
@@ -0,0 +1,47 @@
+use crate::utils::io::{read_line, read_value};
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve29699(reader: &mut impl BufRead, writer: &mut impl Write) {
+    const GIVEN: &str = "WelcomeToSMUPC";
+    let input_index: usize = read_value(read_line(reader));
+
+    let circular_index = (input_index + GIVEN.len() - 1) % GIVEN.len();
+    let character = GIVEN.chars().nth(circular_index).unwrap();
+
+    write!(writer, "{}", character).unwrap();
+}
+
+// https://www.acmicpc.net/problem/29699
+// Welcome to SMUPC!
+#[test]
+fn test1_29699() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "2".to_string(),
+            want: "e".to_string(),
+        },
+        TestData {
+            s: "15".to_string(),
+            want: "W".to_string(),
+        },
+        TestData {
+            s: "140000".to_string(),
+            want: "C".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve29699(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
