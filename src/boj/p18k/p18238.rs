0326f1e5b0151471e63360282b1c57fca1528b50
zrma <bulbitain@gmail.com>
2024-01-20 17:46:31 +0900

    boj: solve 18238 ZOAC 2
    - https://www.acmicpc.net/problem/18238

---
+++ src/boj/p18k/p18238.rs
@@ -0,0 +1,52 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve18238(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let s = read_line(reader);
+    let mut prev = 'A';
+    let mut count = 0;
+
+    for curr in s.chars() {
+        count += distance_between_chars(prev, curr);
+        prev = curr;
+    }
+
+    write!(writer, "{}", count).unwrap();
+}
+
+fn distance_between_chars(from: char, to: char) -> i32 {
+    let diff = (to as i32 - from as i32).abs();
+    diff.min(26 - diff)
+}
+
+// https://www.acmicpc.net/problem/18238
+// noinspection SpellCheckingInspection
+// ZOAC 2
+#[test]
+fn test1_18238() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "ZOAC".to_string(),
+            want: "26".to_string(),
+        },
+        TestData {
+            s: "LBOLVUEEPMOIENMG".to_string(),
+            want: "100".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve18238(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
