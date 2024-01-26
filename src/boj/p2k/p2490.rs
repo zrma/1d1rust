00e52bb6cdd8c433ac9fb78bf2cecbd399b4d08b
zrma <bulbitain@gmail.com>
2024-01-26 15:01:27 +0900

    boj: solve 2490 윷놀이
    - https://www.acmicpc.net/problem/2490

---
+++ src/boj/p2k/p2490.rs
@@ -0,0 +1,71 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve2490(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let mut ans = Vec::with_capacity(3);
+
+    for _ in 0..3 {
+        let input = read_yut_line(reader);
+        ans.push(match input {
+            0 => "D",
+            1 => "C",
+            2 => "B",
+            3 => "A",
+            4 => "E",
+            _ => unreachable!(),
+        });
+    }
+
+    write!(writer, "{}", ans.join("\n")).unwrap();
+}
+
+fn read_yut_line(reader: &mut impl BufRead) -> usize {
+    read_line(reader)
+        .split_whitespace()
+        .take(4)
+        .map(|s| s.parse::<usize>().unwrap())
+        .sum()
+}
+
+// https://www.acmicpc.net/problem/2490
+// 윷놀이
+#[test]
+fn test_solve2490() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "0 1 0 1
+1 1 1 0
+0 0 1 1"
+                .to_string(),
+            want: "B
+A
+B"
+            .to_string(),
+        },
+        TestData {
+            s: "0 0 0 1
+0 0 0 0
+1 1 1 1"
+                .to_string(),
+            want: "C
+D
+E"
+            .to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve2490(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
