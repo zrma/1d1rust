8daf28466a6b7ddbfb94395bbc97c3a6e6e5b94b
zrma <bulbitain@gmail.com>
2024-02-05 08:38:23 +0900

    boj: solve 2257 화학식량
    - https://www.acmicpc.net/problem/2257

---
+++ src/boj/p2k/p2257.rs
@@ -0,0 +1,77 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve2257(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let formula = read_line(reader);
+
+    let total_mass = calculate_mass(&formula);
+
+    write!(writer, "{}", total_mass).unwrap();
+}
+
+fn calculate_mass(formula: &str) -> i32 {
+    let mut stack = Vec::new();
+    for element in formula.chars() {
+        match element {
+            '(' => stack.push(0),
+            ')' => {
+                let mut compound_mass = 0;
+                while let Some(mass) = stack.pop() {
+                    if mass == 0 {
+                        break;
+                    }
+                    compound_mass += mass;
+                }
+                stack.push(compound_mass);
+            }
+            'H' => stack.push(1),
+            'C' => stack.push(12),
+            'O' => stack.push(16),
+            digit => {
+                if let Some(mass) = digit.to_digit(10) {
+                    if let Some(last) = stack.pop() {
+                        stack.push(last * mass as i32);
+                    }
+                }
+            }
+        }
+    }
+
+    stack.iter().sum()
+}
+
+// https://www.acmicpc.net/problem/2257
+// noinspection SpellCheckingInspection
+// 화학식량
+#[test]
+fn test_solve2257() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "(H)2(O)".to_string(),
+            want: "18".to_string(),
+        },
+        TestData {
+            s: "CH(CO2H)3".to_string(),
+            want: "148".to_string(),
+        },
+        TestData {
+            s: "((CH)2(OH2H)(C(H))O)3".to_string(),
+            want: "222".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve2257(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
