e6c1f235bb99eae13f3129338722c331c03299e6
zrma <bulbitain@gmail.com>
2024-02-25 00:07:54 +0900

    utils: add try_next_pos function

--- src/utils/functions.rs
+++ src/utils/functions.rs
@@ -1,3 +1,20 @@
 pub fn check_palindrome_nth(s: &[u8], i: usize) -> bool {
     s[i] == s[s.len() - 1 - i]
 }
+
+pub fn try_next_pos(
+    w: usize,
+    h: usize,
+    x: usize,
+    y: usize,
+    dx: isize,
+    dy: isize,
+) -> Option<(usize, usize)> {
+    let nx = x.checked_add_signed(dx)?;
+    let ny = y.checked_add_signed(dy)?;
+    if nx < w && ny < h {
+        Some((nx, ny))
+    } else {
+        None
+    }
+}
