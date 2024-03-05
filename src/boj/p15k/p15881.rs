7df95ce6e490ad7203f1ec559a8eb5600dc807bd
zrma <bulbitain@gmail.com>
2024-03-06 08:38:45 +0900

    boj: solve 15881 Pen Pineapple Apple Pen
    - https://www.acmicpc.net/problem/15881

---
+++ src/boj/p15k/p15881.rs
@@ -0,0 +1,60 @@
+use crate::utils::io::{read_line, read_value};
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve15881(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let len: usize = read_value(read_line(reader));
+    let line = read_line(reader);
+    let mut count = 0;
+    let mut i = 0;
+    while i <= len - 4 {
+        if &line[i..i + 4] == "pPAp" {
+            count += 1;
+            i += 4;
+        } else {
+            i += 1;
+        }
+    }
+
+    write!(writer, "{}", count).expect("Failed to write");
+}
+
+// https://www.acmicpc.net/problem/15881
+// Pen Pineapple Apple Pen
+#[test]
+fn test_solve15881() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "15
+ApPApPpAPpApPAp"
+                .to_string(),
+            want: "2".to_string(),
+        },
+        TestData {
+            s: "7
+pPApPAp"
+                .to_string(),
+            want: "1".to_string(),
+        },
+        TestData {
+            s: "12
+pPAppPAppPAp"
+                .to_string(),
+            want: "3".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve15881(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).unwrap();
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
