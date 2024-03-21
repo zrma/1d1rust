ce3a9e93c6ba466b1f98af04602149a366d48bef
zrma <bulbitain@gmail.com>
2024-03-21 20:47:43 +0900

    boj: solve 15886 내 선물을 받아줘 2
    - https://www.acmicpc.net/problem/15886

--- src/boj/p15k.rs
+++ src/boj/p15k.rs
@@ -11,6 +11,7 @@ mod p15803;
 mod p15813;
 mod p15814;
 mod p15881;
+mod p15886;
 mod p15894;
 mod p15916;
 mod p15923;
---
+++ src/boj/p15k/p15886.rs
@@ -0,0 +1,51 @@
+use crate::utils::io::{read_line, read_value};
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve15886(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let _: usize = read_value(read_line(reader));
+    let s: String = read_line(reader);
+
+    let count = s
+        .as_bytes()
+        .windows(2)
+        .filter(|&window| window == b"EW")
+        .count();
+
+    write!(writer, "{}", count).expect("Failed to write");
+}
+
+// https://www.acmicpc.net/problem/15886
+// noinspection SpellCheckingInspection
+// 내 선물을 받아줘 2
+#[test]
+fn test_solve15886() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "6
+EEWWEW"
+                .to_string(),
+            want: "2".to_string(),
+        },
+        TestData {
+            s: "5
+EEEEW"
+                .to_string(),
+            want: "1".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = &mut data.s.as_bytes();
+        let mut writer = vec![];
+        solve15886(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).expect("Failed to convert writer to string");
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
