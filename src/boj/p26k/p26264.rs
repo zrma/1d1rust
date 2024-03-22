e35b63e4c1ecef2ba6eeda3d6f444de0ce4a9504
zrma <bulbitain@gmail.com>
2024-03-22 23:19:54 +0900

    boj: solve 26264 빅데이터? 정보보호!
    - https://www.acmicpc.net/problem/26264

---
+++ src/boj/p26k/p26264.rs
@@ -0,0 +1,72 @@
+use crate::utils::io::{read_line, read_value};
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve26264(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let num_of_words: usize = read_value(read_line(reader));
+    let input = read_line(reader);
+    let input = input.as_bytes();
+
+    let mut cnt_of_bigdata = 0;
+    let mut cnt_of_security = 0;
+
+    let mut current_index = 0;
+    while current_index < input.len() && cnt_of_bigdata + cnt_of_security < num_of_words {
+        if input[current_index] == b'b' {
+            cnt_of_bigdata += 1;
+            current_index += "bigdata".len();
+        } else {
+            cnt_of_security += 1;
+            current_index += "security".len();
+        }
+    }
+
+    let ans = match cnt_of_bigdata.cmp(&cnt_of_security) {
+        std::cmp::Ordering::Less => "security!",
+        std::cmp::Ordering::Equal => "bigdata? security!",
+        std::cmp::Ordering::Greater => "bigdata?",
+    };
+
+    write!(writer, "{}", ans).expect("Failed to write");
+}
+
+// https://www.acmicpc.net/problem/26264
+// noinspection SpellCheckingInspection
+// 빅데이터? 정보보호!
+#[test]
+fn test_solve26264() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "5
+securitybigdatasecuritybigdatasecurity"
+                .to_string(),
+            want: "security!".to_string(),
+        },
+        TestData {
+            s: "1
+bigdata"
+                .to_string(),
+            want: "bigdata?".to_string(),
+        },
+        TestData {
+            s: "6
+bigdatabigdatabigdatasecuritysecuritysecurity"
+                .to_string(),
+            want: "bigdata? security!".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = &mut data.s.as_bytes();
+        let mut writer = vec![];
+        solve26264(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).expect("Failed to convert writer to string");
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
