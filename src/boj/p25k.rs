fe15a5c622f57dbf28dcade6764eff6a1e27be39
zrma <bulbitain@gmail.com>
2024-03-24 15:15:42 +0900

    boj: solve 25630 팰린드롬 소떡소떡
    - https://www.acmicpc.net/problem/25630

--- src/boj/p25k.rs
+++ src/boj/p25k.rs
@@ -1,5 +1,6 @@
 mod p25192;
 mod p25314;
+mod p25630;
 mod p25640;
 mod p25757;
 mod p25915;
---
+++ src/boj/p25k/p25630.rs
@@ -0,0 +1,59 @@
+use crate::utils::io::{read_line, read_value};
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve25630(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let len: usize = read_value(read_line(reader));
+    let s = read_line(reader);
+    let s = s.as_bytes();
+
+    let change_count = s
+        .iter()
+        .zip(s.iter().rev())
+        .take(len / 2)
+        .filter(|(a, b)| a != b)
+        .count();
+
+    write!(writer, "{}", change_count).expect("Failed to write");
+}
+
+// https://www.acmicpc.net/problem/25630
+// noinspection SpellCheckingInspection
+// 팰린드롬 소떡소떡
+#[test]
+fn test_solve25630() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "7
+ststtss"
+                .to_string(),
+            want: "2".to_string(),
+        },
+        TestData {
+            s: "8
+ttsststt"
+                .to_string(),
+            want: "1".to_string(),
+        },
+        TestData {
+            s: "6
+ssttss"
+                .to_string(),
+            want: "0".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = &mut data.s.as_bytes();
+        let mut writer = vec![];
+        solve25630(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).expect("Failed to convert writer to string");
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
