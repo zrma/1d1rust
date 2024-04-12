e4cdec3c8d72c451075292b9bf6c91f5000183a6
zrma <bulbitain@gmail.com>
2024-04-13 07:57:52 +0900

    boj: solve 31495 그게 무슨 코드니..
    - https://www.acmicpc.net/problem/31495

---
+++ src/boj/p31k/p31495.rs
@@ -0,0 +1,67 @@
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve31495(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let s = read_line(reader);
+    if s.len() < 3 {
+        write!(writer, "CE").expect("Failed to write");
+        return;
+    }
+
+    match (s.starts_with('"'), s.ends_with('"')) {
+        (true, true) => {
+            write!(writer, "{}", &s[1..s.len() - 1]).expect("Failed to write");
+        }
+        _ => {
+            write!(writer, "CE").expect("Failed to write");
+        }
+    }
+}
+
+// https://www.acmicpc.net/problem/31495
+// noinspection SpellCheckingInspection
+// 그게 무슨 코드니..
+#[test]
+fn test_solve31495() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "\"Hello World".to_string(),
+            want: "CE".to_string(),
+        },
+        TestData {
+            s: "\"Hello World\"".to_string(),
+            want: "Hello World".to_string(),
+        },
+        TestData {
+            s: "\"\"".to_string(),
+            want: "CE".to_string(),
+        },
+        TestData {
+            s: "\"".to_string(),
+            want: "CE".to_string(),
+        },
+        TestData {
+            s: "\"Foo".to_string(),
+            want: "CE".to_string(),
+        },
+        TestData {
+            s: "\"Foo\"".to_string(),
+            want: "Foo".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = &mut data.s.as_bytes();
+        let mut writer = vec![];
+        solve31495(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).expect("Failed to convert writer to string");
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
