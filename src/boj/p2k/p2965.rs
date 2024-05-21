ae2a3aa6bdba786ec73eb53337cedf9ef85a3fad
zrma <bulbitain@gmail.com>
2024-05-22 08:44:53 +0900

    boj: solve 2965 캥거루 세마리
    - https://www.acmicpc.net/problem/2965

---
+++ src/boj/p2k/p2965.rs
@@ -0,0 +1,45 @@
+use crate::read_values_as;
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve2965(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let (a, b, c) = read_values_as!(read_line(reader), i32, i32, i32);
+
+    let ans = (b - a).max(c - b) - 1;
+    write!(writer, "{}", ans).expect("Failed to write");
+}
+
+// https://www.acmicpc.net/problem/2965
+// 캥거루 세마리
+#[test]
+fn test_solve2965() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "2 3 5".to_string(),
+            want: "1".to_string(),
+        },
+        TestData {
+            s: "3 5 9".to_string(),
+            want: "3".to_string(),
+        },
+        TestData {
+            s: "3 5 7".to_string(),
+            want: "1".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = &mut data.s.as_bytes();
+        let mut writer = vec![];
+        solve2965(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).expect("Failed to convert writer to string");
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
