ef0f7aee8bbc262daea6e3108abdea9f118da1c0
zrma <bulbitain@gmail.com>
2024-05-28 08:57:03 +0900

    boj: solve 3507 Automated Telephone Exchange
    - https://www.acmicpc.net/problem/3507

---
+++ src/boj/p3k/p3507.rs
@@ -0,0 +1,57 @@
+use crate::utils::io::{read_line, read_value};
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve3507(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let n: usize = read_value(read_line(reader));
+
+    let result = 199usize.saturating_sub(n);
+
+    write!(writer, "{}", result).expect("Failed to write");
+}
+
+// https://www.acmicpc.net/problem/3507
+// Automated Telephone Exchange
+#[test]
+fn test_solve3507() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "100".to_string(),
+            want: "99".to_string(), // 99, 1 | 98, 2 | ... | 2, 98 | 1, 99
+        },
+        TestData {
+            s: "196".to_string(),
+            want: "3".to_string(), // 99, 97 | 98, 98 | 97, 99
+        },
+        TestData {
+            s: "197".to_string(),
+            want: "2".to_string(), // 99, 98 | 98, 99
+        },
+        TestData {
+            s: "198".to_string(),
+            want: "1".to_string(), // 99, 99
+        },
+        TestData {
+            s: "199".to_string(),
+            want: "0".to_string(),
+        },
+        TestData {
+            s: "239".to_string(),
+            want: "0".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = &mut data.s.as_bytes();
+        let mut writer = vec![];
+        solve3507(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).expect("Failed to convert writer to string");
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
