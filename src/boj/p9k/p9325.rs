3755d762f381c58e476e1de4219af7c10d93861d
zrma <bulbitain@gmail.com>
2024-06-18 22:00:45 +0900

    boj: solve 9325 얼마?
    - https://www.acmicpc.net/problem/9325

---
+++ src/boj/p9k/p9325.rs
@@ -0,0 +1,71 @@
+use crate::read_values_as;
+use crate::utils::io::{read_line, read_value};
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve9325(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let num_cases: usize = read_value(read_line(reader));
+
+    let ans = (0..num_cases)
+        .map(|_| {
+            let s: i32 = read_value(read_line(reader));
+            let n: i32 = read_value(read_line(reader));
+            let total = (0..n)
+                .map(|_| {
+                    let (q, p) = read_values_as!(read_line(reader), i32, i32);
+                    q * p
+                })
+                .sum::<i32>()
+                + s;
+            total.to_string()
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    write!(writer, "{}", ans).expect("Failed to write");
+}
+
+// https://www.acmicpc.net/problem/9325
+// 얼마?
+#[test]
+fn test_solve9325() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "2
+10000
+2
+1 2000
+3 400
+50000
+0"
+            .to_string(),
+            want: "13200
+50000"
+                .to_string(),
+        },
+        TestData {
+            s: "1
+100
+3
+1 100
+2 200
+3 300"
+                .to_string(),
+            want: "1500".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = &mut data.s.as_bytes();
+        let mut writer = vec![];
+        solve9325(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).expect("Failed to convert writer to string");
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
