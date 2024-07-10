0fea03942d9b201c44f57c380cf5a3ab2ad1fc25
zrma <bulbitain@gmail.com>
2024-07-10 09:30:01 +0900

    boj: solve 12756 고급 여관
    - https://www.acmicpc.net/problem/12756

---
+++ src/boj/p12k/p12756.rs
@@ -0,0 +1,62 @@
+use crate::read_values_as;
+use crate::utils::io::read_line;
+use std::cmp::Ordering;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve12756(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let (a_atk, a_hp) = read_values_as!(read_line(reader), i32, i32);
+    let (b_atk, b_hp) = read_values_as!(read_line(reader), i32, i32);
+
+    let a_surv_turns = (a_hp + b_atk - 1) / b_atk;
+    let b_surv_turns = (b_hp + a_atk - 1) / a_atk;
+
+    let ans = match a_surv_turns.cmp(&b_surv_turns) {
+        Ordering::Less => "PLAYER B",
+        Ordering::Equal => "DRAW",
+        Ordering::Greater => "PLAYER A",
+    };
+
+    write!(writer, "{}", ans).expect("Failed to write");
+}
+
+// https://www.acmicpc.net/problem/12756
+// 고급 여관
+#[test]
+fn test_solve12756() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+
+    for (i, data) in [
+        TestData {
+            s: "4 12
+5 5"
+            .to_string(),
+            want: "PLAYER A".to_string(),
+        },
+        TestData {
+            s: "4 12
+12 4"
+                .to_string(),
+            want: "DRAW".to_string(),
+        },
+        TestData {
+            s: "4 12
+4 13"
+                .to_string(),
+            want: "PLAYER B".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve12756(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).expect("Failed to convert writer to string");
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
