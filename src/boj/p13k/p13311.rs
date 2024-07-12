09cc054e8f948929e09cf32e694f11436c8485f6
zrma <bulbitain@gmail.com>
2024-07-13 01:57:38 +0900

    boj: solve 13311 행운의 편지
    - https://www.acmicpc.net/problem/13311

---
+++ src/boj/p13k/p13311.rs
@@ -0,0 +1,31 @@
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve13311(_: &mut impl BufRead, writer: &mut impl Write) {
+    write!(writer, "-1").expect("Failed to write");
+}
+
+// https://www.acmicpc.net/problem/13311
+// 행운의 편지
+#[test]
+fn test_solve13311() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+
+    for (i, data) in [TestData {
+        s: "".to_string(),
+        want: "-1".to_string(),
+    }]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve13311(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).expect("Failed to convert writer to string");
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
