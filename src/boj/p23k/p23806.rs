934b3816bfdf4598562969acac2c5a0879b13192
zrma <bulbitain@gmail.com>
2024-08-29 08:24:09 +0900

    boj: solve 23806 골뱅이 찍기 - ㅁ
    - https://www.acmicpc.net/problem/23806

---
+++ src/boj/p23k/p23806.rs
@@ -0,0 +1,82 @@
+use crate::utils::io::{read_line, read_value};
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve23806(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let n: usize = read_value(read_line(reader));
+
+    let border_line = "@".repeat(n * 5);
+    let middle_line = format!("{}{}{}", "@".repeat(n), " ".repeat(n * 3), "@".repeat(n));
+
+    let mut ans = String::with_capacity(5 * n * 5 * n);
+
+    for _ in 0..n {
+        ans.push_str(&border_line);
+        ans.push('\n');
+    }
+
+    for _ in 0..(3 * n) {
+        ans.push_str(&middle_line);
+        ans.push('\n');
+    }
+
+    for _ in 0..n {
+        ans.push_str(&border_line);
+        ans.push('\n');
+    }
+
+    ans.pop(); // remove last '\n'
+
+    write!(writer, "{}", ans).expect("Failed to write");
+}
+
+// https://www.acmicpc.net/problem/23806
+// 골뱅이 찍기 - ㅁ
+#[test]
+fn test_solve23806() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+
+    for (i, data) in [
+        TestData {
+            s: "1".to_string(),
+            want: "@@@@@
+@   @
+@   @
+@   @
+@@@@@"
+                .to_string(),
+        },
+        TestData {
+            s: "3".to_string(),
+            want: "@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@
+@@@         @@@
+@@@         @@@
+@@@         @@@
+@@@         @@@
+@@@         @@@
+@@@         @@@
+@@@         @@@
+@@@         @@@
+@@@         @@@
+@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@"
+                .to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve23806(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).expect("Failed to convert writer to string");
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
