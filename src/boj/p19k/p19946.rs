851d2feb5fa2bf021bd78d0ec03b132aa9204b44
zrma <bulbitain@gmail.com>
2024-09-29 00:22:22 +0900

    boj: solve 19946 2의 제곱수 계산하기
    - https://www.acmicpc.net/problem/19946

---
+++ src/boj/p19k/p19946.rs
@@ -0,0 +1,42 @@
+use crate::utils::io::{read_line, read_value};
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve19946(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let n: u128 = read_value(read_line(reader));
+    let extra_operations = n.trailing_zeros(); // 실수 이후 추가 연산 횟수
+    let ans = 64 - extra_operations; // 실수가 처음 발생한 위치
+    write!(writer, "{}", ans).unwrap();
+}
+
+// https://www.acmicpc.net/problem/19946
+// 2의 제곱수 계산하기
+#[test]
+fn test_solve19946() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "18446744073709551615".to_string(),
+            want: "64".to_string(),
+        },
+        TestData {
+            s: "18446744073709551614".to_string(),
+            want: "63".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        {
+            let mut reader = &mut data.s.as_bytes();
+            let mut writer = vec![];
+            solve19946(&mut reader, &mut writer);
+
+            let got = String::from_utf8(writer).expect("Failed to convert writer to string");
+            assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+        }
+    }
+}
