2480738aaeb7c9621436f488ae8093492c2d2ba6
zrma <bulbitain@gmail.com>
2024-10-28 09:18:57 +0900

    boj: solve 23971 ZOAC 4
    - https://www.acmicpc.net/problem/23971

---
+++ src/boj/p23k/p23971.rs
@@ -0,0 +1,61 @@
+use crate::read_values_as;
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve23971(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let (h, w, n, m) = read_values_as!(read_line(reader), u32, u32, u32, u32);
+
+    let rows = (h + n) / (n + 1);
+    let cols = (w + m) / (m + 1);
+    let ans = rows * cols;
+
+    write!(writer, "{}", ans).expect("write failed");
+}
+
+// https://www.acmicpc.net/problem/23971
+// noinspection SpellCheckingInspection
+// ZOAC 4
+#[test]
+fn test_solve23971() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "5 4 1 1".to_string(),
+            want: "6".to_string(),
+        },
+        TestData {
+            s: "4 4 1 1".to_string(),
+            want: "4".to_string(),
+        },
+        TestData {
+            s: "3 3 1 1".to_string(),
+            want: "4".to_string(),
+        },
+        TestData {
+            s: "4 3 1 1".to_string(),
+            want: "4".to_string(),
+        },
+        TestData {
+            s: "5 3 1 1".to_string(),
+            want: "6".to_string(),
+        },
+        TestData {
+            s: "5 4 2 2".to_string(),
+            want: "4".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve23971(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).expect("writer should be a valid string");
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
