34d2049039a14657e4426d20fd9ba925ca145f9c
zrma <bulbitain@gmail.com>
2024-11-06 08:57:08 +0900

    boj: solve 28014 첨탑 밀어서 부수기
    - https://www.acmicpc.net/problem/28014

---
+++ src/boj/p28k/p28014.rs
@@ -0,0 +1,67 @@
+use crate::utils::io::{read_line, read_n_values, read_value};
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve28014(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let n: usize = read_value(read_line(reader));
+    let heights: Vec<i32> = read_n_values(reader, n);
+
+    let ans = heights
+        .iter()
+        .fold((0, 0), |(count, prev), &curr| {
+            if curr >= prev {
+                (count + 1, curr)
+            } else {
+                (count, curr)
+            }
+        })
+        .0;
+
+    write!(writer, "{}", ans).expect("write! should work");
+}
+
+// https://www.acmicpc.net/problem/28014
+// 첨탑 밀어서 부수기
+#[test]
+fn test_solve28014() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "6
+1 3 2 5 8 1"
+                .to_string(),
+            want: "4".to_string(),
+        },
+        TestData {
+            s: "8
+1 2 3 4 5 6 7 8"
+                .to_string(),
+            want: "8".to_string(),
+        },
+        TestData {
+            s: "8
+8 7 6 5 4 3 2 1"
+                .to_string(),
+            want: "1".to_string(),
+        },
+        TestData {
+            s: "8
+7 7 6 5 4 3 2 1"
+                .to_string(),
+            want: "2".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve28014(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).expect("writer should be a valid string");
+        assert_eq!(got, data.want, "failed at {} with {}", i, data.s);
+    }
+}
