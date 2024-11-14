8a461946c7e8251c85f583da6b2db38069cd1d3d
zrma <bulbitain@gmail.com>
2024-11-14 09:18:33 +0900

    boj: solve 29729 가변 배열
    - https://www.acmicpc.net/problem/29729

--- src/boj/p29k.rs
+++ src/boj/p29k.rs
@@ -2,6 +2,7 @@ mod p29197;
 mod p29699;
 mod p29720;
 mod p29722;
+mod p29729;
 mod p29731;
 mod p29751;
 mod p29766;
---
+++ src/boj/p29k/p29729.rs
@@ -0,0 +1,93 @@
+use crate::read_values_as;
+use crate::utils::io::{read_line, read_value};
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve29729(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let (initial_max, n, m) = read_values_as!(read_line(reader), usize, usize, usize);
+
+    let mut max = initial_max;
+    let mut curr = 0;
+
+    for _ in 0..(n + m) {
+        let op: usize = read_value(read_line(reader));
+
+        if op == 1 {
+            curr += 1;
+            if curr > max {
+                max *= 2;
+            }
+        } else {
+            curr -= 1;
+        }
+    }
+
+    writeln!(writer, "{}", max).expect("write should work");
+}
+
+// https://www.acmicpc.net/problem/29729
+// 가변 배열
+#[test]
+fn test_solve29729() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "1 5 1
+1
+1
+1
+1
+0
+1"
+            .to_string(),
+            want: "4".to_string(),
+        },
+        TestData {
+            s: "10 12 0
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1"
+            .to_string(),
+            want: "20".to_string(),
+        },
+        TestData {
+            s: "1 12 0
+            1
+            1
+            1
+            1
+            1
+            1
+            1
+            1
+            1
+            1
+            1
+            1"
+            .to_string(),
+            want: "16".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = &mut data.s.as_bytes();
+        let mut writer = vec![];
+        solve29729(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).expect("writer should be a valid string");
+        assert_eq!(got.trim(), data.want, "failed at {} with {}", i, data.s);
+    }
+}
