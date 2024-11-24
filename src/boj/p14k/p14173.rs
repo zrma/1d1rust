2a589136cd1b72e63b683f8718b5e31b6481f3a3
zrma <bulbitain@gmail.com>
2024-11-24 21:06:57 +0900

    boj: solve 14173 Square Pasture
    - https://www.acmicpc.net/problem/14173

---
+++ src/boj/p14k/p14173.rs
@@ -0,0 +1,75 @@
+use crate::read_values_as;
+use crate::utils::io::read_line;
+use std::io::{BufRead, Write};
+
+#[allow(dead_code)]
+fn solve14173(reader: &mut impl BufRead, writer: &mut impl Write) {
+    let (x1, y1, x2, y2): (i32, i32, i32, i32) =
+        read_values_as!(read_line(reader), i32, i32, i32, i32);
+    let (x3, y3, x4, y4): (i32, i32, i32, i32) =
+        read_values_as!(read_line(reader), i32, i32, i32, i32);
+
+    let (min_x, min_y, max_x, max_y) = bounding_box(x1, y1, x2, y2, x3, y3, x4, y4);
+
+    let side = (max_x - min_x).max(max_y - min_y);
+    let area = side * side;
+
+    writeln!(writer, "{}", area).expect("writeln! should work");
+}
+
+fn bounding_box(
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+    x3: i32,
+    y3: i32,
+    x4: i32,
+    y4: i32,
+) -> (i32, i32, i32, i32) {
+    let min_x = x1.min(x3);
+    let min_y = y1.min(y3);
+    let max_x = x2.max(x4);
+    let max_y = y2.max(y4);
+    (min_x, min_y, max_x, max_y)
+}
+
+// https://www.acmicpc.net/problem/14173
+// Square Pasture
+#[test]
+fn test_solve14173() {
+    struct TestData {
+        s: String,
+        want: String,
+    }
+    for (i, data) in [
+        TestData {
+            s: "6 6 8 8
+1 8 4 9"
+                .to_string(),
+            want: "49".to_string(),
+        },
+        TestData {
+            s: "0 0 10 10
+5 5 6 6"
+                .to_string(),
+            want: "100".to_string(),
+        },
+    ]
+    .iter()
+    .enumerate()
+    {
+        let mut reader = data.s.as_bytes();
+        let mut writer = vec![];
+        solve14173(&mut reader, &mut writer);
+
+        let got = String::from_utf8(writer).expect("writer should be a valid string");
+        assert_eq!(
+            got.trim(),
+            data.want.trim(),
+            "failed at {} with {}",
+            i,
+            data.s
+        );
+    }
+}
